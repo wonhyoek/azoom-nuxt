11665065379f6b3d2dd0dbfaf6cefc509f97e751
wonhyoek <epahsejtmxm@naver.com>
2024-10-03 21:04:56 +0900

    韓国語 -> 日本語に変更

--- pages/index.vue
+++ pages/index.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-    <h1>ToDo 리스트</h1>
-    <input v-model="newTodo" placeholder="할 일 입력" />
-    <button @click="addTodo">추가</button>
+    <h1>ToDo List</h1>
+    <input v-model="newTodo" placeholder="やることを入力" />
+    <button @click="addTodo">追加</button>
 
     <ul>
       <li v-for="(todo, index) in todos" :key="index">
         <span :style="{ textDecoration: todo.completed ? 'line-through' : 'none' }">
           {{ todo.text }}
         </span>
-        <button @click="toggleTodo(index)">완료</button>
-        <button @click="removeTodo(index)">삭제</button>
+        <button @click="toggleTodo(index)">完了</button>
+        <button @click="removeTodo(index)">削除</button>
       </li>
     </ul>
   </div>
@@ -20,7 +20,7 @@
 import { ref } from 'vue'
 import { useTodoStore } from '~/stores/todo'
 
-const newTodo = ref<string>('') // TypeScript로 string 타입 지정
+const newTodo = ref<string>('')
 const todoStore = useTodoStore()
 
 const addTodo = () => {
